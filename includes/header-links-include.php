15858ecc87e02f63066041a917f624984704ac22
Zephyri0s <pacadelta@gmail.com>
2024-11-01 12:40:47 +0000

    new bootstrap link

--- includes/header-links-include.php
+++ includes/header-links-include.php
@@ -1,7 +1,7 @@
 <!-- imports cart -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
 <link rel="stylesheet" href="css/mobile.css">
 <link rel="stylesheet" href="css/desktop.css" media="only screen and (min-width : 720px)">
