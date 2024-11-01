afde7504d573ca75b69820dc0bfd7bc38af60f5b
Zephyri0s <pacadelta@gmail.com>
2024-11-01 12:01:38 +0000

    changed includes formmating

--- checkout-page.php
+++ checkout-page.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Checkout Page</title>
-    <link rel="stylesheet" href="css/mobile.css">
-    <link rel="stylesheet" href="css/desktop.css" media="only screen and (min-width : 720px)">
-    <?php include "connection.php";?>
+    <?php 
+        include "includes/connection.php";
+        include "includes/header-links-include.php";
+    ?>
+
 </head>
 <body>
     <?php 
---
+++ includes/header-links-include.php
@@ -0,0 +1,9 @@
+<!-- imports cart -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+<link rel="stylesheet" href="css/mobile.css">
+<link rel="stylesheet" href="css/desktop.css" media="only screen and (min-width : 720px)">
+
+<script src="js/main.js" defer></script>
