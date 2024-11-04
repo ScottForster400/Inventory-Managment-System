2b7ca8d1c31da213c07f3a005e2d2f4540280ef7
Zephyri0s <pacadelta@gmail.com>
2024-11-04 14:05:20 +0000

    i hate sqlite

--- login-inc.php
+++ login-inc.php
@@ -1,6 +1,34 @@
 <?php
-    if(isset($_POST["Submit"])) {
-        require_once "includes/connection.php";
-        
+    if(isset($_POST["submit"])) {
+        include "includes/connection.php";
+        $EmailInput = $_POST["email"];
+        $PasswordInput = $_POST["password"];
+        $id = 1;
+
+        if (!filter_var($EmailInput, FILTER_VALIDATE_EMAIL)){
+            header("Location: login.php?error=InvalidEmail");         
+        }
+
+        $stmt = $db->prepare("SELECT * FROM Employee WHERE employee_id = 1");
+        //$stmt->bindParam("i", $id);
+        var_dump($EmailInput);
+        var_dump($PasswordInput);
+        var_dump($stmt);
+        $stmt->execute();
+        var_dump($EmailInput);
+        $Results = $stmt->fetchColumn();
+        if ($Results->numRows > 0) {
+            while($employee = $Results->fetch_object()) {
+                $DbPWD = $employee->password;
+                // if (password_verify($PasswordInput, $DbPWD) == true) {
+                    var_dump($EmailInput);
+                    $id = $employee->employee_id;
+                    session_start();
+                    $_SESSION['EmployeeUID'] = $id;
+                    header('Location: index.php'); 
+                    exit();    
+                // }
+            }
+        }
     }
 ?>
