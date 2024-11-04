083db0347b906f4efa5055de2a50093381e1092d
matkuz161 <1panmateuszkuzon@gmail.com>
2024-11-04 14:55:32 +0000

    reports

--- admin-generate-report.php
+++ admin-generate-report.php
@@ -17,13 +17,16 @@
 
 
     $sql = "SELECT *
-    FROM Transactions";
+    FROM Transactions
+    LEFT JOIN Product ON (Transactions.product_ID = Product.product_ID)
+    ";
 
     $statement = $db->query($sql);
 ?>
 
 
 <table class="table">
+
   <thead>
     <tr>
       <th scope="col">Month</th>
@@ -32,39 +35,23 @@
     </tr>
   </thead>
   <tbody class="table-group-divider">
+    <?php
+      while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+    ?>
     <tr>
-      <th scope="row">October 2024</th>
+      <th scope="row"><?php echo $row["date_of_sale"]?></th>
       
-      <th>Gooner</th>
+      <th><?php echo $row["name"]?>: <?php echo $row["price"]?></th>
 
     </tr>
+    <?php
+      }
+    ?>
   </tbody>
+
 </table>
 
 
-<div class="flex-table">
-        <?php
-        while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
-        ?>
-        <div class="card" style="width: 18rem; margin-bottom: 50px;">
-            <div class="card-body">
-                <h5 class="card-title"><b><?php echo $row["employee_id"] . " Year " . $row["product_id"]; ?></b></h5>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item"><b>Price: </b><?php echo $row["price"]; ?></li>
-                    <li class="list-group-item"><b>Date: </b><?php echo $row["date_of_sale"]; ?></li>
-                </ul>
-                <br>
-                <form method="post" action="">
-                    <input type="hidden" name="product_id" value="<?php echo $row['product_id']; ?>">
-                    <button type="submit" name="compare" class="btn btn-primary">Compare</button>
-                </form>
-            </div>
-        </div>
-        <?php
-        }
-        ?>
-
-  </div>
 
 
 
