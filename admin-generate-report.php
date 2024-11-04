3e52b676193fe619b34bedc319ef36855f2d6912
matkuz161 <1panmateuszkuzon@gmail.com>
2024-11-04 14:05:36 +0000

    transactions

--- admin-generate-report.php
+++ admin-generate-report.php
@@ -12,6 +12,16 @@
 <body>
 <?php include("includes/nav-include.php");?>
 
+<?php
+
+
+
+    $sql = "SELECT *
+    FROM Transactions";
+
+    $statement = $db->query($sql);
+?>
+
 
 <table class="table">
   <thead>
@@ -24,49 +34,37 @@
   <tbody class="table-group-divider">
     <tr>
       <th scope="row">October 2024</th>
-      <td>
-        <div class="modal fade" id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabindex="-1">
-          <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h1 class="modal-title fs-5" id="exampleModalToggleLabel">Monthly Report</h1>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-              </div>
-              <div class="modal-body">
-                Show a second modal and hide this one with the button below.
-              </div>
-              <div class="modal-footer">
-                <button class="btn btn-primary" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal">Open second modal</button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="modal fade" id="exampleModalToggle2" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2" tabindex="-1">
-          <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h1 class="modal-title fs-5" id="exampleModalToggleLabel2">Graph Based Report</h1>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-              </div>
-              <div class="modal-body">
-                Hide this modal and show the first with the button below.
-              </div>
-              <div class="modal-footer">
-                <button class="btn btn-primary" data-bs-target="#exampleModalToggle" data-bs-toggle="modal">Back to first</button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <button class="btn btn-primary" data-bs-target="#exampleModalToggle" data-bs-toggle="modal">Generate Report</button>
-      </td>
+      
+      <th>Gooner</th>
 
     </tr>
   </tbody>
 </table>
 
 
+<div class="flex-table">
+        <?php
+        while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+        ?>
+        <div class="card" style="width: 18rem; margin-bottom: 50px;">
+            <div class="card-body">
+                <h5 class="card-title"><b><?php echo $row["employee_id"] . " Year " . $row["product_id"]; ?></b></h5>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item"><b>Price: </b><?php echo $row["price"]; ?></li>
+                    <li class="list-group-item"><b>Date: </b><?php echo $row["date_of_sale"]; ?></li>
+                </ul>
+                <br>
+                <form method="post" action="">
+                    <input type="hidden" name="product_id" value="<?php echo $row['product_id']; ?>">
+                    <button type="submit" name="compare" class="btn btn-primary">Compare</button>
+                </form>
+            </div>
+        </div>
+        <?php
+        }
+        ?>
 
-
+  </div>
 
 
 
