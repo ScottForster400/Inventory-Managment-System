8f8b934caaa91c754bd0f5997793dc0c469b1961
matkuz161 <1panmateuszkuzon@gmail.com>
2024-11-04 13:14:38 +0000

    modal

--- admin-generate-report.php
+++ admin-generate-report.php
@@ -17,24 +17,49 @@
   <thead>
     <tr>
       <th scope="col">Month</th>
-      <th scope="col">Generate Report</th>
+      <th scope="col">Reports</th>
 
     </tr>
   </thead>
   <tbody class="table-group-divider">
     <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
+      <th scope="row">October 2024</th>
+      <td>
+        <div class="modal fade" id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabindex="-1">
+          <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalToggleLabel">Monthly Report</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                Show a second modal and hide this one with the button below.
+              </div>
+              <div class="modal-footer">
+                <button class="btn btn-primary" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal">Open second modal</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="modal fade" id="exampleModalToggle2" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2" tabindex="-1">
+          <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalToggleLabel2">Graph Based Report</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                Hide this modal and show the first with the button below.
+              </div>
+              <div class="modal-footer">
+                <button class="btn btn-primary" data-bs-target="#exampleModalToggle" data-bs-toggle="modal">Back to first</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <button class="btn btn-primary" data-bs-target="#exampleModalToggle" data-bs-toggle="modal">Generate Report</button>
+      </td>
 
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td> Gooner</td>
     </tr>
   </tbody>
 </table>
--- js/main.js
+++ js/main.js
@@ -2,3 +2,7 @@ function burgerMenu() {
     var offScreenMenu = document.querySelector('.offscreen-menu');
     offScreenMenu.classList.toggle('active');
   };
+
+  $('myModal').on('shown.bs.modal', function (){
+    $('#myInput').trigger('focus')
+  })
