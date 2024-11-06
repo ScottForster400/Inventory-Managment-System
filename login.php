9ad16ccc8b05f2d1bd9bcbff24e85c2af48c47a4
Naz2833 <149158744+Naz2833@users.noreply.github.com>
2024-11-06 11:18:04 +0000

    switch css to mobile

--- login.php
+++ login.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gouner Games</title>
-    <link rel="stylesheet" href="css/desktop.css">
+    <link rel="stylesheet" href="css/mobile.css">
 
 </head>
 
